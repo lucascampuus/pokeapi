4c8e4dde888dda68ed62fcb5cba74cd5c54a51cd
Lucas Luiz Campos <108141148+lucascampuus@users.noreply.github.com>
2022-06-25 21:54:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+<head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
 <title>PokeAPI</title>
-
-<!-- Custom fonts for this template-->
 <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-<!-- Custom styles for this template-->
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 <link href="css/sb-admin-2.min.css" rel="stylesheet">
 <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-</head>
-    
-    <body id="page-top">
-
-<!-- Page Wrapper -->
-<div id="wrapper"> 
-      
+</head> 
+<body id="page-top">
+<div id="wrapper">   
       <!-- Sidebar -->
       <?php include "sidebar.php"; ?>
-      <!-- End of Sidebar --> 
-      
-      <!-- Content Wrapper -->
       <div id="content-wrapper" class="d-flex flex-column"> 
-    
-    <!-- Main Content -->
-    <div id="content"> 
-          
+    <div id="content">      
           <!-- Topbar -->
           <?php include "header.php";?>
-          
-          <!-- End of Topbar --> 
-          
-          <!-- Begin Page Content -->
           <div class="container-fluid"> 
-        
-        <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
               <h1 class="h3 mb-0 text-gray-800">My Favorites</h1>
             </div>
-        
-        <!-- Content Row -->
         <div class="row">
               <?php
               for ( $id = 0; $id < 8; $id++ ) {
@@ -55,7 +31,6 @@
                 $dados = file_get_contents( 'https://pokeapi.co/api/v2/pokemon/' . $idx[ $id ] . '/' );
                 $pokemon = json_decode( $dados );
                 ?>
-              <!-- Earnings (Monthly) Card Example -->
               <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                   <div class="card-body">
@@ -71,23 +46,17 @@
               </div>
                 </div>
           </div>
-              <?php } ?>
-              
-
+              <?php } ?>           
             </div>
         
         <div class="row"> 
-              <!-- Content Column -->
               <div class="col-lg-12 mb-4"> 
-            
-            <!-- Project Card Example -->
             <div class="card shadow mb-4">
                   <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Stats Pokemons by habitat</h6>
               </div>
                   <div class="card-body">
                 <?php
-
                 $dados = file_get_contents( 'https://pokeapi.co/api/v2/pokemon-habitat' );
                 $habitats = json_decode( $dados );
                 $y = 0;
@@ -108,46 +77,26 @@
                     $x++;
                   }
                   $x = round( ( $x * 100 ) / $y, 1 );
-
-
                   echo '<h4 class="small font-weight-bold">' . $results->name . '<span class="float-right">' . $x . '%</span></h4>
                 <div class="progress mb-4">
                       <div class="progress-bar bg-dark" role="progressbar" style="width: ' . $x . '%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>';
-
                 }
                 ?>
               </div>
                 </div>
-            
-            <!-- Color System --> 
-            
           </div>
-            </div>
-      </div>
-          <!-- /.container-fluid --> 
-          
         </div>
-    <!-- End of Main Content --> 
-    
-    <!-- Footer -->
-    <footer class="sticky-footer bg-white">
+      </div>         
+   </div>
+   <footer class="sticky-footer bg-white">
           <div class="container my-auto">
         <div class="copyright text-center my-auto"> <span>Copyright &copy; PokeAPI2022</span> </div>
       </div>
-        </footer>
-    <!-- End of Footer --> 
-    
+   </footer>
   </div>
-      <!-- End of Content Wrapper --> 
-      
-    </div>
-<!-- End of Page Wrapper --> 
-
-<!-- Scroll to Top Button--> 
+</div>
 <a class="scroll-to-top rounded" href="#page-top"> <i class="fas fa-angle-up"></i> </a> 
-
-<!-- Logout Modal-->
 <div class="modal fade" id="searchmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static" data-keyboard="false">
       <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document"">
     <div class="modal-content">
@@ -155,7 +104,7 @@
           <div class="modal-footer">
         <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
       </div>
-        </div>
+    </div>
   </div>
 </div>
 
