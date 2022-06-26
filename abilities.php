76c0fd91dc5da142d70f3c34bb06f6b648ee1d14
Lucas Luiz Campos <108141148+lucascampuus@users.noreply.github.com>
2022-06-25 21:50:15 -0300

    Update abilities.php

--- abilities.php
+++ abilities.php
@@ -8,57 +8,31 @@
 <meta name="author" content="">
 <title>PokeAPI</title>
 
-<!-- Custom fonts for this template-->
 <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-<!-- Custom styles for this template-->
 <link href="css/sb-admin-2.min.css" rel="stylesheet">
 <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
-
-<!-- Page Wrapper -->
 <div id="wrapper"> 
-  
-  <!-- Sidebar -->
   <?php include "sidebar.php"; ?>
-  <!-- End of Sidebar --> 
-  
-  <!-- Content Wrapper -->
   <div id="content-wrapper" class="d-flex flex-column"> 
-    
-    <!-- Main Content -->
     <div id="content"> 
-      
-      <!-- Topbar -->
       <?php include "header.php";?>
-      
-      <!-- End of Topbar --> 
-      
-      <!-- Begin Page Content -->
       <div class="container-fluid"> 
-        
-        <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
           <h1 class="h3 mb-0 text-gray-800">Favorites Pokemons</h1>
         </div>
-        
-        <!-- Content Row -->
         <div class="row">
           <?php
-
           $dados = file_get_contents( 'https://pokeapi.co/api/v2/pokemon-habitat' );
           $type = json_decode( $dados );
-
           foreach ( $type->results as $results ) {
-
-
             ?>
-          <!-- Earnings (Monthly) Card Example -->
           <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
               <div class="card-body">
@@ -76,30 +50,16 @@
           </div>
           <?php }?>
         </div>
-      </div>
-      <!-- /.container-fluid --> 
-      
+      </div>  
     </div>
-    <!-- End of Main Content --> 
-    
-    <!-- Footer -->
     <footer class="sticky-footer bg-white">
       <div class="container my-auto">
         <div class="copyright text-center my-auto"> <span>Copyright &copy; Your Website 2021</span> </div>
       </div>
     </footer>
-    <!-- End of Footer --> 
-    
   </div>
-  <!-- End of Content Wrapper --> 
-  
 </div>
-<!-- End of Page Wrapper --> 
-
-<!-- Scroll to Top Button--> 
 <a class="scroll-to-top rounded" href="#page-top"> <i class="fas fa-angle-up"></i> </a> 
-
-<!-- Logout Modal-->
 <div class="modal fade" id="abilitypokemon" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
   <div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
@@ -118,21 +78,11 @@
     </div>
   </div>
 </div>
-
-<!-- Bootstrap core JavaScript--> 
 <script src="vendor/jquery/jquery.min.js"></script> 
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
-
-<!-- Core plugin JavaScript--> 
 <script src="vendor/jquery-easing/jquery.easing.min.js"></script> 
-
-<!-- Custom scripts for all pages--> 
 <script src="js/sb-admin-2.min.js"></script> 
-
-<!-- Page level plugins --> 
 <script src="vendor/chart.js/Chart.min.js"></script> 
-
-<!-- Page level custom scripts --> 
 <script src="js/demo/chart-area-demo.js"></script> 
 <script src="js/demo/chart-pie-demo.js"></script> 
 <script src="vendor/chart.js/Chart.min.js"></script> 
